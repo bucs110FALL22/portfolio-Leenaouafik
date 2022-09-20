3658528401fa91afc81fecba22d2d504b31792e7
Leena Ouafik <112440101+Leenaouafik@users.noreply.github.com>
2022-09-20 02:45:24 +0000

    guessing numbers

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -1,12 +1,13 @@
 import random 
 my_nums= random.randrange(1,11)
-num_guesses= 0
+correct_num= False
 
-for i in range[3]: 
+
+for i in range(5): 
   guess= int(input("Guess a number:"))
-  if result > guess: 
-    print("too low")
+  if guess > my_nums:
+    print("your guess too high")
+  elif guess < my_nums:
+    print("your guess is too low")
   else: 
-    print ("too high")
-  if result == guess: 
     print("correct!")
